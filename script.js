d1b3c33f6921921afa6873061c72e50ba19e6ee8
Olanrewaju Ayoade <121438359+Ayolademi@users.noreply.github.com>
2023-03-27 22:55:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const ratingBtns = document.querySelectorAll(".rating__button button");
+const firstBtn = ratingBtns[ratingBtns.length - 1]
+const firstListItem = firstBtn.parentElement
+const lastBtn = ratingBtns[0]
+const lastListItem = lastBtn.parentElement
+const submitBtn = document.querySelector(".submit-button")
